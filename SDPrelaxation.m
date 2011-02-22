38876bfd7752e809adbead6a531e23dd87a56c36
Arash Rouhani <miffoljud@gmail.com>
2011-02-22 14:05:12 +0100

    Started a bit on SDPrelaxation.m

--- SDPrelaxation.m
+++ SDPrelaxation.m
@@ -2,4 +2,15 @@ function [s, c]=SDPrelaxation(W)
 
 n = size(W);
 
+cvx_begin
+    variable X(n, n) symmetric
+    minimize ( trace(W*X) )
+    subject to
+        diag(X) == 1;
+        X >= 0;
+
+cvx_end
+
+V = chol(X, 'lower');
+
 c = calcCost(W, s);
