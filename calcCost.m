9b2bf24c9d74be41258d248f11f39b183f4fbdee
Arash Rouhani <miffoljud@gmail.com>
2011-02-22 11:05:22 +0100

    Implemented calcCost, tested a bit

--- calcCost.m
+++ calcCost.m
@@ -4,6 +4,6 @@ n = size(W);
 c = 0;
 for i = 1:n 
     for j = 1:n 
-        c = c + W(i, j);
+        c = c + W(i, j)*(1-s(i)*s(j))*0.5 ;
     end    
 end
--- random_assign.m
+++ random_assign.m
@@ -1,7 +1,7 @@
 function [s, c]=random_assign(W)
 
 n = size(W);
-s = zeros(n);
+s = zeros(n, 1);
 for i = 1:n 
     s(i) = 2*((rand > 0.5) - 0.5);
 end
